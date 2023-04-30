f3f797fded0c21b8c8d6b6e7891e81fc0b6f5f04
fliix <43872194+fliix-de@users.noreply.github.com>
2023-04-30 16:45:51 +0200

    Delete Create-UDG-VM-from-VHDX.ps1

--- Hyper-V/Ubuntu/New-VM/Create-UDG-VM-from-VHDX.ps1
+++
@@ -1,8 +0,0 @@
-$vmName = "Ubuntu 18.04"
-$fqdn = "ubuntu-1804"
-$rootPassword = "<PASSWORD>"
-$VMProcessorCount = 2
-$MemoryStartupBytes = 1GB
-$VMSwitchName = 'vSwitchExternal'
-
-.\New-VMFromVHDX.ps1 -VMName $vmName -FQDN $fqdn -RootPassword $rootPassword -MemoryStartupBytes $MemoryStartupBytes -VMProcessorCount $VMProcessorCount -VMSwitchName $VMSwitchName
