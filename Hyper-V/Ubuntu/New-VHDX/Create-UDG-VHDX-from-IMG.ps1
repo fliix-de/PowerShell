cb77f15a3c6ed2c1c96f5cc552ccd4bf8a0f98bb
fliix <43872194+fliix-de@users.noreply.github.com>
2023-04-30 16:45:33 +0200

    Delete Create-UDG-VHDX-from-IMG.ps1

--- Hyper-V/Ubuntu/New-VHDX/Create-UDG-VHDX-from-IMG.ps1
+++
@@ -1,9 +0,0 @@
-$imgFile = ".\ubuntu-18.04-server-cloudimg-amd64.img"
-$vmName = "Ubuntu 18.04"
-$fqdn = "ubuntu-1804"
-$rootPassword = "<PASSWORD>"
-$VHDXSizeBytes = 60GB
-
-choco install qemu-img -y
-
-.\New-VHDXFromUbuntuImage.ps1 -SourcePath $imgFile -VMName $vmName -FQDN $fqdn -RootPassword $rootPassword -VHDXSizeBytes $VHDXSizeBytes
